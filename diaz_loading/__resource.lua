c4f028f124a7b2dc75282db8eb7d32717f2b49e6
Diaz GOD <77013049+DiazGOD@users.noreply.github.com>
2021-05-23 19:12:28 +0300

    diaz_loading
    Loading screen for Fivem

---
+++ diaz_loading/__resource.lua
@@ -0,0 +1,12 @@
+--Diaz GOD
+files {
+    'index.html',
+    'style.css',
+    'logo.png',
+    'diaz-music.mp3',
+    'script.js'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
